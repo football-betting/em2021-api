a0627a5a9d1dd05c89730252d23335b94b843419
Rafał Wesołowski <r.wesolowski@nexus-united.com>
2021-06-05 00:58:39 +0200

    Saint Pipeline, please give me the green light

--- json_api.md
+++ json_api.md
@@ -256,7 +256,104 @@ example
 }
 ```
 
+### User all past tips
+
+Uri: `/api/user_tip/past/{username}`
+Method: `GET`
+
+##### Response:
+
+```json
+{
+  "success": true,
+  "data": {
+    "name": "ninja",
+    "position": 1,
+    "scoreSum": 24,
+    "tips": [
+      {
+        "matchId": "2000-06-16:2100:FR-DE",
+        "matchDatetime": "2000-06-16 21:00",
+        "tipTeam1": 2,
+        "tipTeam2": 3,
+        "scoreTeam1": 1,
+        "scoreTeam2": 4,
+        "team1": "FR",
+        "team2": "DE",
+        "score": 2
+      },
+      {
+        "matchId": "2021-06-04:2254:IT-SP",
+        "matchDatetime": "2021-06-04 22:54",
+        "tipTeam1": 1,
+        "tipTeam2": 0,
+        "scoreTeam1": 1,
+        "scoreTeam2": 4,
+        "team1": "IT",
+        "team2": "SP",
+        "score": 0
+      },
+      {
+        "matchId": "2021-06-04:2255:PR-AU",
+        "matchDatetime": "2021-06-04 22:55",
+        "team1": "PR",
+        "team2": "AU",
+        "scoreTeam1": null,
+        "scoreTeam2": null,
+        "tipTeam1": null,
+        "tipTeam2": null,
+        "score": null
+      }
+    ]
+  }
+}
+```
+
+Test: _tests/Acceptance/Component/UserTips/Infrastructure/UserTipsControllerTest.php_
 
 
 
+### User all future tips
+
+Uri: `/api/user_tip/future`
+Method: `GET`
+
+##### Response:
+
+```json
+{
+  "success": true,
+  "data": {
+    "name": "ninja",
+    "position": 1,
+    "scoreSum": 24,
+    "tips": [
+      {
+        "matchId": "2021-06-04:2258:CZ-NL",
+        "matchDatetime": "2021-06-04 22:58",
+        "tipTeam1": 4,
+        "tipTeam2": 5,
+        "team1": "CZ",
+        "team2": "NL",
+        "scoreTeam1": null,
+        "scoreTeam2": null,
+        "score": null
+      },
+      {
+        "matchId": "2999-06-20:1800:RU-EN",
+        "matchDatetime": "2999-06-20 18:00",
+        "tipTeam1": 4,
+        "tipTeam2": 2,
+        "team1": "RU",
+        "team2": "EN",
+        "scoreTeam1": null,
+        "scoreTeam2": null,
+        "score": null
+      }
+    ]
+  }
+}
+```
+
+Test: _tests/Acceptance/Component/UserTips/Infrastructure/UserTipsControllerTest.php_
 
--- tests/Acceptance/Component/UserTips/Infrastructure/UserTipsControllerTest.php
+++ tests/Acceptance/Component/UserTips/Infrastructure/UserTipsControllerTest.php
@@ -79,11 +79,11 @@ class UserTipsControllerTest extends WebTestCase
         $contents = json_decode($response->getContent(), true);
 
         self::assertTrue($contents['success']);
-        self::assertArrayHasKey('data',$contents);
+        self::assertArrayHasKey('data', $contents);
 
         $data = $contents['data'];
 
-        self::assertArrayHasKey('tips',$data);
+        self::assertArrayHasKey('tips', $data);
 
         $tips = $data['tips'];
         self::assertCount(5, $tips);
@@ -113,5 +113,149 @@ class UserTipsControllerTest extends WebTestCase
         self::assertSame('PR', $tip['team1']);
         self::assertSame('AU', $tip['team2']);
         self::assertNull($tip['score']);
+    }
+
+    public function testUserPastTip()
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = static::$container->get(UserRepository::class);
+        $customerUser = $userRepository->find(2);
+
+
+        $this->client->request(
+            'GET',
+            '/api/user_tip/past/' . UserTips::USER,
+            [],
+            [],
+            [
+                'Authorization' => $customerUser->getToken()
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(200);
+
+        $response = $this->client->getResponse();
+
+        self::assertTrue($response->headers->contains('Content-Type', 'application/json'));
+
+        $contents = json_decode($response->getContent(), true);
+
+        self::assertTrue($contents['success']);
+        self::assertArrayHasKey('data', $contents);
+
+        $data = $contents['data'];
+
+        self::assertArrayHasKey('tips', $data);
+
+        $tips = $data['tips'];
+        self::assertCount(3, $tips);
+
+        self::assertCount(9, $tips[0]);
+        self::assertCount(9, $tips[1]);
+        self::assertCount(9, $tips[2]);
+
+        $tip = $tips[0];
+        self::assertSame('2000-06-16:2100:FR-DE', $tip['matchId']);
+        self::assertSame('2000-06-16 21:00', $tip['matchDatetime']);
+        self::assertSame(2, $tip['tipTeam1']);
+        self::assertSame(3, $tip['tipTeam2']);
+        self::assertSame(1, $tip['scoreTeam1']);
+        self::assertSame(4, $tip['scoreTeam2']);
+        self::assertSame('FR', $tip['team1']);
+        self::assertSame('DE', $tip['team2']);
+        self::assertSame(2, $tip['score']);
+
+        $tip = $tips[2];
+        self::assertNull($tip['tipTeam1']);
+        self::assertNull($tip['tipTeam2']);
+        self::assertNull($tip['score']);
+        self::assertNull($tip['scoreTeam1']);
+        self::assertNull($tip['scoreTeam2']);
+    }
+
+    public function testUserFutureTip()
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = static::$container->get(UserRepository::class);
+        $customerUser = $userRepository->find(1);
+
+
+        $this->client->request(
+            'GET',
+            '/api/user_tip/future',
+            [],
+            [],
+            [
+                'Authorization' => $customerUser->getToken(),
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(200);
+
+        $response = $this->client->getResponse();
+
+        self::assertTrue($response->headers->contains('Content-Type', 'application/json'));
+
+        $contents = json_decode($response->getContent(), true);
+
+        self::assertTrue($contents['success']);
+        self::assertArrayHasKey('data', $contents);
+
+        $data = $contents['data'];
+
+        self::assertArrayHasKey('tips', $data);
+
+        $tips = $data['tips'];
+        self::assertCount(2, $tips);
+
+        self::assertCount(9, $tips[0]);
+        self::assertCount(9, $tips[1]);
+
+        $tip = $tips[0];
+        self::assertSame(4, $tip['tipTeam1']);
+        self::assertSame(5, $tip['tipTeam2']);
+        self::assertNull($tip['scoreTeam1']);
+        self::assertNull($tip['scoreTeam2']);
+        self::assertSame('CZ', $tip['team1']);
+        self::assertSame('NL', $tip['team2']);
+        self::assertNull($tip['score']);
+
+        $tip = $tips[1];
+        self::assertSame('RU', $tip['team1']);
+        self::assertSame('EN', $tip['team2']);
+        self::assertSame(4, $tip['tipTeam1']);
+        self::assertSame(2, $tip['tipTeam2']);
+        self::assertNull($tip['score']);
+        self::assertNull($tip['scoreTeam1']);
+        self::assertNull($tip['scoreTeam2']);
+    }
+
+    public function testNotFoundUserPastTip()
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = static::$container->get(UserRepository::class);
+        $customerUser = $userRepository->find(2);
+
+
+        $this->client->request(
+            'GET',
+            '/api/user_tip/past/not_found_user',
+            [],
+            [],
+            [
+                'Authorization' => $customerUser->getToken(),
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(500);
+
+        $response = $this->client->getResponse();
+
+        self::assertTrue($response->headers->contains('Content-Type', 'application/json'));
+
+        $contents = json_decode($response->getContent(), true);
+
+        self::assertSame('Syntax error', $contents['message']);
+        self::assertFalse($contents['success']);
     }
 }
