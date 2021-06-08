50867c89c5ba7113ead3fc81de118324bf274f63
Phillip Schleicher <p.schleicher@nexus-united.com>
2021-06-08 19:16:49 +0200

    Update init

--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-composer install
+composer install -o
 
-php bin/console doctrine:database:create --no-interaction
+php bin/console doctrine:database:create --if-not-exists --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
-
-#php bin/console doctrine:database:create --no-interaction --env=test
-#php bin/console doctrine:migrations:migrate --no-interaction --env=test
+php bin/console messenger:consume app.to.tip --no-interaction --time-limit=0.1 > /dev/null 2>&1
 
 
