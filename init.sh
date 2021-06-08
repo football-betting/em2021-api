35a3fc6946f8a94c4f592e7538000aece5abbd12
Phillip Schleicher <p.schleicher@nexus-united.com>
2021-06-08 18:03:07 +0200

    Commented lines on init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ composer install
 php bin/console doctrine:database:create --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 
-php bin/console doctrine:database:create --no-interaction --env=test
-php bin/console doctrine:migrations:migrate --no-interaction --env=test
+#php bin/console doctrine:database:create --no-interaction --env=test
+#php bin/console doctrine:migrations:migrate --no-interaction --env=test
 
 
